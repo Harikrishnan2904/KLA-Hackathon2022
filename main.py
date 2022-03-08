a9dbf2ffe204bc4eb0e509b0402eaacb46c5bff0
hari <harikrishnan29042001@gmail.com>
2022-03-08 14:00:00 +0530

    Milesstone 2A

--- main.py
+++ main.py
@@ -2,17 +2,61 @@ import time
 import yaml
 import threading
 import datetime
+import pandas as pd
 
-ms = '1B'           #Milestone number
+outputs = {}
+
+ms = '2A'           #Milestone number
 
 file1 = open("Milestone"+ms+".txt","w")
 
-def timeFunction(inputs, name):
+def checkCondition(condition):
+    if len(condition):
+        s = e = 0
+        n = 0
+        con = ''
+        for i in range(len(condition)):
+            if condition[i] == '(':
+                s = i
+            elif condition[i] == ')':
+                e = i
+            if e and condition[i]==' ':
+                n = i
+            if condition[i] == '>' or condition[i] == '<':
+                con = condition[i]
+        conName = condition[s+1:e-12]
+        val = int(condition[n+1:])
+        #print(condition,conName,con,val)
+        while conName not in outputs.keys():
+            continue
+        if (con=='>' and outputs[conName][1]>val) or (con=='<' and outputs[conName][1]<val):
+            return True
+        else:
+            return False
+    else:
+        return True
+    
+def dataLoad(inputs, name, condition):
+    inputFile = inputs["Filename"]
+    file1.write(str(datetime.datetime.now())+ ';'+ name+ " Entry\n")
+    if checkCondition(condition):
+        file1.write(str(datetime.datetime.now())+ ';'+ name+ " Executing DataLoad ("+ inputFile+")\n")    
+        df = pd.read_csv("Milestone"+ms[0]+"/"+inputFile)
+        outputs[name] = [df,df.shape[0]]
+    else:
+        file1.write(str(datetime.datetime.now())+ ';'+ name+ " Skipped\n")
+    file1.write(str(datetime.datetime.now())+ ';'+ name+ " Exit\n")
+
+def timeFunction(inputs, name, condition):
+    #print(name, inputs, condition)
     funcInput = inputs["FunctionInput"]
     t = inputs["ExecutionTime"]
     file1.write(str(datetime.datetime.now())+ ';'+ name+ " Entry\n")
-    file1.write(str(datetime.datetime.now())+ ';'+ name+ " Executing TimeFunction ("+ funcInput+ ', '+ t+ ")\n")
-    time.sleep(int(t))
+    if checkCondition(condition):
+        file1.write(str(datetime.datetime.now())+ ';'+ name+ " Executing TimeFunction ("+ funcInput+ ', '+ t+ ")\n")
+        time.sleep(int(t))
+    else:
+        file1.write(str(datetime.datetime.now())+ ';'+ name+ " Skipped\n")
     file1.write(str(datetime.datetime.now())+ ';'+ name+ " Exit\n")
 
 def flow(workflow,name,isSequential):
@@ -26,8 +70,13 @@ def flow(workflow,name,isSequential):
                 else:
                     flow(workflow[k]["Activities"],sname,False)
             elif workflow[k]["Type"] == "Task":
-                if workflow[k]["Function"] == "TimeFunction":
-                    timeFunction(workflow[k]["Inputs"],sname)
+                condition = ""
+                if "Condition" in workflow[k].keys():
+                    condition += workflow[k]["Condition"]
+                if workflow[k]["Function"] == "TimeFunction":                  
+                    timeFunction(workflow[k]["Inputs"],sname, condition)
+                elif workflow[k]["Function"] == "DataLoad":
+                    dataLoad(workflow[k]["Inputs"],sname, condition)
     else:                                       #CONCURRENT
         threadList = []
         for k in workflow.keys():
@@ -38,8 +87,13 @@ def flow(workflow,name,isSequential):
                 else:
                     threadList.append(threading.Thread(target=flow,args=(workflow[k]["Activities"],sname,False)))
             elif workflow[k]["Type"] == "Task":
+                condition = ""
+                if "Condition" in workflow[k].keys():
+                    condition += workflow[k]["Condition"]
                 if workflow[k]["Function"] == "TimeFunction":
-                    threadList.append(threading.Thread(target=timeFunction,args=(workflow[k]["Inputs"],sname)))
+                    threadList.append(threading.Thread(target=timeFunction,args=(workflow[k]["Inputs"],sname,condition)))
+                elif workflow[k]["Function"] == "DataLoad":
+                    threadList.append(threading.Thread(target=dataLoad,args=(workflow[k]["Inputs"],sname,condition)))
         for th in threadList:
             th.start()
         for th in threadList:
